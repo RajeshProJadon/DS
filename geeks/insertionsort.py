a9c54ba406d9b5de8ea28223043642c26ee24deb
Angular <rajeshmcaacet@gmail.com>
2020-01-31 10:18:44 +0530

    INsertion sort

--- geeks/insertionsort.py
+++ geeks/insertionsort.py
@@ -14,23 +14,19 @@ print("Applying insertion sort for sorting on array elements: ")
 
 def insertion_sort(arrList):
     for i in range(1, n):
-        j = i - 1
         next_element = arrList[i]
+        j = i
     
     # Compare the current element with the next element
 
-    while (arrList[j] > next_element) and (j >= 0):
-        arrList[j+1] =arrList[j]
-        j = j -1
-    arrList[j+1] = next_element
+        while (arrList[j-1] > next_element) and (j > 0):
+            arrList[j] =arrList[j-1]
+            j = j -1
+        arrList[j] = next_element
 
     print ("After sortinng array elements are: ", end='')
     for j in range(0,n):
         print(arrList[j] + " ", end='')
-# def printArray(arrList):
-#     print ("After sortinng array elements are: ", end='')
-#     for i in range(0,n):
-#         print(arrList[i] + " ", end='')
+
 
 insertion_sort(arr)
-# printArray(arr)
